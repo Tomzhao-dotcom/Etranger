e132cca63bf0b017c4266cbf4026ea1ee564969c
赵芮 <zhaorui01@inspur.com>
2021-04-16 15:45:11 +0800

    修改

--- Etranger-web/src/main/java/com/etranger/web/controller/source/PartnerController.java
+++ Etranger-web/src/main/java/com/etranger/web/controller/source/PartnerController.java
@@ -13,6 +13,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
